2853832b808fbae9cf2d15c2d92635359b4f8b38
kanta <c0a25098d1@edu.teu.ac.jp>
2026-04-14 17:25:25 +0900

    課題3

---
+++ flying_kokaton.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    bg_img2 = pg.transform.flip(bg_img, True,False)#練習8
+    kk_img = pg.image.load("fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect()#練習10: こうかとんRectの取得
+    kk_rct.center = 300,200 #練習10: こうかとんの初期座標の表示
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        x = 0
+        y = 0
+        key_lst = pg.key.get_pressed()
+        x = 0
+        y = 0 #すべてのキーの押下状態の取得
+        if key_lst[pg.K_UP]:
+            y = -1
+        if key_lst[pg.K_DOWN]:
+            y = 1
+        if key_lst[pg.K_LEFT]:
+            x = -1
+        if key_lst[pg.K_RIGHT]:
+            x = 2
+        kk_rct.move_ip(x,y)
+        
+        x = tmr%3200 #練習5
+        screen.blit(bg_img, [-x, 0]) #練習2 #練習6
+        screen.blit(bg_img2, [-x+1600, 0]) #練習7
+        screen.blit(bg_img, [-x+3200, 0]) #練習9
+        screen.blit(kk_img, kk_rct) #練習4
+        kk_rct.move_ip((-1, 0)) #課題1
+        pg.display.update()
+        tmr += 1        
+
+        clock.tick(200)#練習6
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
